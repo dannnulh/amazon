b302a08f9ca02718d5ae7b17bbcbb235f32b8213
Jeffrey Hu <zhiwehu@gmail.com>
2017-05-22 14:19:36 +0800

    BROKER_URL = env('CELERY_BROKER_URL', default='django://')

--- config/settings/base.py
+++ config/settings/base.py
@@ -270,6 +270,7 @@ AUTOSLUG_SLUGIFY_FUNCTION = 'slugify.slugify'
 ########## CELERY
 INSTALLED_APPS += ['src.taskapp.celery.CeleryConfig']
 CELERY_BROKER_URL = env('CELERY_BROKER_URL', default='django://')
+BROKER_URL = env('CELERY_BROKER_URL', default='django://')
 if CELERY_BROKER_URL == 'django://':
     CELERY_RESULT_BACKEND = 'redis://'
 else:
