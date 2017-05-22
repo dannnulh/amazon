50661f207eec0e6974fecc28f971439ee7cba42c
Jeffrey Hu <zhiwehu@gmail.com>
2017-05-22 13:29:37 +0800

    @user_passes_test(lambda u: u.is_staff)

--- src/amazon/views.py
+++ src/amazon/views.py
@@ -1,10 +1,12 @@
 from django.conf import settings
+from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import redirect
 
 from .models import Item
 from .tasks import amazon_item_review_task, amazon_items_task
 
 
+@user_passes_test(lambda u: u.is_staff)
 def get_items(request):
     if settings.DEBUG:
         amazon_items_task()
@@ -14,6 +16,7 @@ def get_items(request):
     return redirect('home')
 
 
+@user_passes_test(lambda u: u.is_staff)
 def get_reviews(request):
     item_id = request.GET.get('item_id', None)
     try:
