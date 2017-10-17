633a4d192e422cb0d8be4b98c08ead432906d480
Jeffrey Hu <zhiwehu@gmail.com>
2017-10-17 11:07:48 +0800

    get orders and display orders

--- src/amazon/utils.py
+++ src/amazon/utils.py
@@ -325,7 +325,7 @@ def save_orders(orders):
 
 
 def _get_orders_by_next_token(x, token, orders):
-    sleep(20)
+    sleep(30)
     resp = x.list_orders_by_next_token(token)
     data = resp.parsed
     for order in data['Orders']['Order']:
